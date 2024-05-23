14ff5b457b03ad7d03a6d6b5917d26306f871e6a
Samuel Hearn <scchearn@gmail.com>
2024-05-23 13:50:20 +0200

    add(keymap): added keymap to close buffers

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,6 +7,9 @@ local keymap = vim.keymap
 -- Select all
 keymap.set("n", "<C-a>", "gg<S-v>G", { desc = "Select all" })
 
+-- Close all buffers
+keymap.set("n", "<leader>ba", ":bufdo bd<CR>", { desc = "Close all buffers" })
+
 -- Normal mode comment toggle
 -- keymap.set("n", "<leader>.", "gcc", { noremap = true, silent = true, desc = "Toggle comment" })
 
