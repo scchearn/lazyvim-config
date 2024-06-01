712fd5fb328ac6819ee125174eb676d81aa514ab
Samuel Hearn <scchearn@gmail.com>
2024-06-01 15:23:33 +0200

    change(maps): added 'jj' as Esc sequence

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,11 +4,14 @@
 
 local keymap = vim.keymap
 
+-- Escape
+keymap.set("i", "jj", "<Esc>", { noremap = false, desc = "Escape" })
+
 -- Select all
 keymap.set("n", "<C-a>", "gg<S-v>G", { desc = "Select all" })
 
 -- Close all buffers
-keymap.set("n", "<leader>ba", ":bufdo bd<CR>", { desc = "Close all buffers" })
+keymap.set("n", "<leader>ba", ":%bd<enter>", { desc = "Close all buffers" })
 
 -- Normal mode comment toggle
 -- keymap.set("n", "<leader>.", "gcc", { noremap = true, silent = true, desc = "Toggle comment" })
