92eb645eb9dc268d447b66875fcaac472c53253a
Samuel Hearn <scchearn@gmail.com>
2024-06-18 11:32:41 +0200

    fix(codeium): now works in cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,18 +1,25 @@
 return {
-  -- "hrsh7th/nvim-cmp",
-  -- lazy = false,
-  -- priority = 100,
-  -- dependencies = {
-  --   "onsails/lspkind-nvim",
-  --   "hrsh7th/cmp-nvim-lsp",
-  --   "hrsh7th/cmp-path",
-  --   "hrsh7th/cmp-buffer",
-  --   "saadparwaiz1/cmp_luasnip",
-  -- },
-  -- config = function()
-  --   require("config.completion")
-  -- end,
+  "hrsh7th/nvim-cmp",
+  ---@param opts cmp.ConfigSchema
+  opts = function(_, opts)
+    table.insert(opts.sources, { name = "codeium" })
+  end,
 }
+
+-- "hrsh7th/nvim-cmp",
+-- lazy = false,
+-- priority = 100,
+-- dependencies = {
+--   "onsails/lspkind-nvim",
+--   "hrsh7th/cmp-nvim-lsp",
+--   "hrsh7th/cmp-path",
+--   "hrsh7th/cmp-buffer",
+--   "saadparwaiz1/cmp_luasnip",
+-- },
+-- config = function()
+--   require("config.completion")
+-- end,
+--
 -- cmp.lua
 -- local cmp = require("cmp")
 -- local window = require("cmp.config.window")
