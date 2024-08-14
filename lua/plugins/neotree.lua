29ab89027546de898c65b86226ea2962ed10de79
Samuel Hearn <scchearn@gmail.com>
2024-08-14 09:23:09 +0200

    add(neotree): keymap to toggle neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   keys = {
-    { "<leader>e", "<cmd>Neotree focus<cr>", desc = "Explorer NeoTree Toggle", remap = true },
+    { "<leader>e", "<cmd>Neotree focus<cr>", desc = "Explorer NeoTree Focus", remap = true },
+    { "\\", "<cmd>Neotree toggle<cr>", desc = "Explorer NeoTree Toggle", remap = true },
   },
 }
