e1e47fb3b83bb30c45cba048b48711546c58b204
Samuel Hearn <scchearn@gmail.com>
2024-09-23 13:55:00 +0200

    remove(codecase): disable code case for now.

--- lua/plugins/codecase.lua
+++ lua/plugins/codecase.lua
@@ -1,9 +1,9 @@
 return {
-  "nicwest/vim-camelsnek",
-  event = "LazyFile",
-  keys = {
-    { "<leader>pk", "<cmd>Kebab<cr>", desc = "Convert to kebab-case" },
-    { "<leader>ps", "<cmd>Snek<cr>", desc = "Convert to snake-case" },
-    { "<leader>pc", "<cmd>CamelB<cr>", desc = "Convert to camel-case" },
-  },
+  -- "nicwest/vim-camelsnek",
+  -- event = "LazyFile",
+  -- keys = {
+  --   { "<leader>pk", "<cmd>Kebab<cr>", desc = "Convert to kebab-case" },
+  --   { "<leader>ps", "<cmd>Snek<cr>", desc = "Convert to snake-case" },
+  --   { "<leader>pc", "<cmd>CamelB<cr>", desc = "Convert to camel-case" },
+  -- },
 }
