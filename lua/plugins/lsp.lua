22a3309ce42ac449d785fa4078c80a234f1b986e
Samuel Hearn <scchearn@gmail.com>
2024-12-02 08:46:31 +0200

    add(lsp): leader c-o organise imports.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,21 @@
 return {
-  -- "neovim/nvim-lspconfig",
-  -- opts = {
-  --   servers = {
-  --   },
-  -- },
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      svelte = {
+        keys = {
+          {
+            "<leader>co",
+            LazyVim.lsp.action["source.organizeImports"],
+            desc = "Organize Imports",
+          },
+        },
+        capabilities = {
+          workspace = {
+            didChangeWatchedFiles = vim.fn.has("nvim-0.10") == 0 and { dynamicRegistration = true },
+          },
+        },
+      },
+    },
+  },
 }
