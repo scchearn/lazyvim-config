34b74d5a40f3b6c191bdf4dc078b87be7969a112
Samuel Hearn <scchearn@gmail.com>
2025-03-27 15:43:28 +0200

    add(config): dap

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -41,6 +41,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.coding.neogen" },
+    -- dap
+    { import = "lazyvim.plugins.extras.dap.core" },
     { import = "plugins" },
   },
   defaults = {
